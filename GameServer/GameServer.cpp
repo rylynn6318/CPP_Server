8a720948637fc020a1872b17f7e4d0fa80e14c1f
donghyeon <gg6318@naver.com>
2021-11-28 23:34:32 +0900

    condition_variable

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -10,18 +10,23 @@ std::mutex m;
 std::queue<int32> q;
 HANDLE handle;
 
+//CV는 User-Level Object
+std::condition_variable cv;
+
 void Producer()
 {
 	while (true)
 	{
+		//1) Lock을 잡고
+		//2) 공유 변수 값을 수정
+		//3) Lock을 풀고
+		//4) 조건변수 통해 다른 쓰레드에게 통지
 		{
 			std::unique_lock<std::mutex> lock(m);
 			q.push(100);
 		}
 
-		::SetEvent(handle);
-
-		std::this_thread::sleep_for(10000ms);
+		cv.notify_one(); //wait중인 쓰레드가 있으면 딱 1개를 깨운다.
 	}
 }
 
@@ -29,14 +34,21 @@ void Consumer()
 {
 	while (true)
 	{
-		::WaitForSingleObject(handle, INFINITE);
-
 		std::unique_lock<std::mutex> lock(m);
-		if (q.empty() == false)
+		cv.wait(lock, []() {return q.empty() == false; });
+		//1) Lock을 잡고
+		//2) 조건 확인
+		//	조건 만족 -> 빠져 나와서 코드를 진행
+		//	조건 만족 X -> Lock을 풀어주고 대기 상태
+
+		//그런데 notify_one을 했으면 항상 조건식을 만족하는거 아닐까?
+		//Spurius Wakeup
+		//notify_one을 할 때 lock을 잡고 있는 것이 아니기 때문
+
 		{
 			int32 data = q.front();
 			q.pop();
-			std::cout << data << endl;
+			std::cout << q.size() << std::endl;
 		}
 	}
 }
