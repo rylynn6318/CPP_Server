2124c03019cf961a6068379befd8f78cf82a18b8
donghyeon <gg6318@naver.com>
2021-11-29 00:16:43 +0900

    future

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,65 +4,74 @@
 #include <atomic>
 #include <thread>
 #include <mutex>
-#include <windows.h>
+#include <future>
 
-std::mutex m;
-std::queue<int32> q;
-HANDLE handle;
+int64 result;
 
-//CV는 User-Level Object
-std::condition_variable cv;
-
-void Producer()
+int64 Calcuate()
 {
-	while (true)
-	{
-		//1) Lock을 잡고
-		//2) 공유 변수 값을 수정
-		//3) Lock을 풀고
-		//4) 조건변수 통해 다른 쓰레드에게 통지
-		{
-			std::unique_lock<std::mutex> lock(m);
-			q.push(100);
-		}
+	int64 sum = 0;
 
-		cv.notify_one(); //wait중인 쓰레드가 있으면 딱 1개를 깨운다.
+	for (int32 i = 0; i < 100'000; i++)
+	{
+		sum += i;
 	}
+
+	return sum;
 }
 
-void Consumer()
+void PromiseWorker(std::promise<std::string>&& promise)
 {
-	while (true)
-	{
-		std::unique_lock<std::mutex> lock(m);
-		cv.wait(lock, []() {return q.empty() == false; });
-		//1) Lock을 잡고
-		//2) 조건 확인
-		//	조건 만족 -> 빠져 나와서 코드를 진행
-		//	조건 만족 X -> Lock을 풀어주고 대기 상태
+	promise.set_value("Secret Message"s);
+}
 
-		//그런데 notify_one을 했으면 항상 조건식을 만족하는거 아닐까?
-		//Spurius Wakeup
-		//notify_one을 할 때 lock을 잡고 있는 것이 아니기 때문
-
-		{
-			int32 data = q.front();
-			q.pop();
-			std::cout << q.size() << std::endl;
-		}
-	}
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+	task();
 }
 
 int main()
 {
+	//동기 실행
+	//int64 sum = Calcuate();
+	//std::cout << sum << std::endl;
 
-	handle = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+	//std::future
+	{
+		//1) deferred -> lazy evaluation 지연해서 실행하세요
+		//2) async -> 별도의 쓰레드를 만들어서 실행하세요
+		//3) deferred | async -> 둘 중 알아서 골라주세요
+		std::future<int64> future = std::async(std::launch::async, Calcuate);
 
-	std::thread t1(Producer);
-	std::thread t2(Consumer);
+		//TODO
 
-	t1.join();
-	t2.join();
+		int64 sum = future.get();
+	}
 
-	::CloseHandle(handle);
+	//std::promise
+	{
+		//미래에 결과물을 반환해줄거라 약속
+		std::promise<std::string> promise;
+		std::future<std::string> future = promise.get_future();
+
+		std::thread t(PromiseWorker, std::move(promise));
+
+		std::string message = future.get();
+		std::cout << message << std::endl;
+
+		t.join();
+	}
+
+	//std::packged_task
+	{
+		std::packaged_task<int64(void)> task(Calcuate);
+		std::future<int64> future = task.get_future();
+
+		std::thread t(TaskWorker, std::move(task));
+
+		int64 sum = future.get();
+		std::cout << sum << std::endl;
+
+		t.join();
+	}
 }
