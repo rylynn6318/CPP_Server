6d28048ded68ec442cd91726062ea2a3d19b74fb
donghyeon <gg6318@naver.com>
2022-04-16 02:04:51 +0900

    포맷 수정

--- ServerCore/Allocator.h
+++ ServerCore/Allocator.h
@@ -3,24 +3,24 @@
 class BaseAllocator
 {
 public:
-	static auto Alloc(int32 size)->void*;
-	static auto Release(void* ptr)->void;
+	static auto Alloc(int32 size) -> void*;
+	static auto Release(void* ptr)-> void;
 };
 
 class StompAllocator
 {
 	enum { PAGE_SIZE = 0x1000 };
 public:
-	static auto Alloc(int32 size)->void*;
-	static auto Release(void* ptr)->void;
+	static auto Alloc(int32 size) -> void*;
+	static auto Release(void* ptr) -> void;
 };
 
 
 class PoolAllocator
 {
 public:
-	static auto Alloc(int32 size)->void*;
-	static auto Release(void* ptr)->void;
+	static auto Alloc(int32 size) -> void*;
+	static auto Release(void* ptr) -> void;
 };
 
 
@@ -35,20 +35,20 @@ public:
 	template<typename Other>
 	StlAllocator(const StlAllocator<Other>&) {}
 
-	T* allocate(size_t count)
+	auto allocate(size_t count) -> T*
 	{
 		const int32 size = static_cast<int32>(count * sizeof(T));
 		return static_cast<T*>(PoolAllocator::Alloc(size));
 	}
 
-	void deallocate(T* ptr, size_t count)
+	auto deallocate(T* ptr, size_t count) -> void
 	{
 		PoolAllocator::Release(ptr);
 	}
 
 	template<typename U>
-	bool operator==(const StlAllocator<U>&) { return true; }
+	auto operator==(const StlAllocator<U>&) -> bool { return true; }
 
 	template<typename U>
-	bool operator!=(const StlAllocator<U>&) { return false; }
+	auto operator!=(const StlAllocator<U>&) -> bool { return false; }
 };
